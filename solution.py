7e7bd43dd78f6f53719ea9360ff836121f1d4a25
MichaelStroganov <90139406+MichaelStroganov@users.noreply.github.com>
2023-04-11 20:36:59 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import math
 
 
 chat_id = 1004085803 # Ваш chat ID, не меняйте название переменной
@@ -8,7 +9,12 @@ def solution(x_success: int,
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-  if y_success/x_success > y_cnt/x_cnt:
-    return True
-  elif y_success/x_success <= y_cnt/x_cnt:
+  alpha = 0.03
+  p1 = y_success/x_success
+  p2 = y_cnt/x_cnt
+  z = (p1 - p2) / math.sqrt(p1*(1-p1)/x_success + p2*(1-p2)/y_success)
+  z_alpha = abs(norm.ppf(alpha/2))
+  if abs(z) > z_alpha:
     return False
+  else:
+    return True
