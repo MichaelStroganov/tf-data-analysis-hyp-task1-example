8f4896b4e766b30a270e268d6626a24386961340
MichaelStroganov <90139406+MichaelStroganov@users.noreply.github.com>
2023-04-11 22:23:33 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -10,9 +10,9 @@ def solution(x_success: int,
              y_success: int, 
              y_cnt: int) -> bool:
   alpha = 0.03
-  p1 = y_success/x_success
-  p2 = y_cnt/x_cnt
-  z = (p1 - p2) / math.sqrt(p1*(1-p1)/x_success + p2*(1-p2)/y_success)
+  p1 = x_success/x_cnt
+  p2 = y_success/y_cnt
+  z = (p1 - p2)/math.sqrt(p1*(1-p1)/x_cnt + p2*(1-p2)/y_cnt)
   z_alpha = abs(norm.ppf(alpha/2))
   if abs(z) > z_alpha:
     return True
